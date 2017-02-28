7a594956a868d0b5f5055574f0fff3ab9a54b1aa
Ryan <ryan314r@gmail.com>
2017-02-28 14:32:50 -0600

    Initial commit

---
+++ brick-breaker/life.js
@@ -0,0 +1,29 @@
+// get the canvas and drawing context
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+
+// draw a rectange
+function rect(x, y, width, height, color) {
+	// start a new stroke so when we fill in the rectange everything
+	// else we have drawn already will not be redrawn
+	ctx.beginPath();
+
+	// add a rectange to the path
+	ctx.rect(x, y, width, height);
+
+	// set the color for the rectange
+	ctx.fillStyle = color;
+
+	// draw the rectange
+	ctx.fill();
+}
+
+if(typeof draw == "function") {
+	function frame() {
+		draw();
+
+		requestAnimationFrame(frame)
+	}
+
+	frame();
+}
